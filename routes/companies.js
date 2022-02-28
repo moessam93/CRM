0fecd59fd4712b6ab04e1722a8d8fcd94796921c
Mohamed Essam <moessam@Mohameds-Air.home>
2022-02-28 23:35:33 +0200

    add isAuth middlewares to apis

--- routes/companies.js
+++ routes/companies.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const { isAuth } = require('../config/passport');
 const router = express.Router();
-
 const { getAllCompanies, createCompany, getCompanyByID, updateCompanyByID, deleteCompanyByID } = require('../controllers/companies');
 
-router.route('/api/companies').get(getAllCompanies).post(createCompany);
-router.route('/api/companies/:id').get(getCompanyByID).patch(updateCompanyByID).delete(deleteCompanyByID);
+router.route('/api/companies').get(isAuth,getAllCompanies).post(isAuth,createCompany);
+router.route('/api/companies/:id').get(isAuth,getCompanyByID).patch(isAuth,updateCompanyByID).delete(isAuth,deleteCompanyByID);
 
 module.exports = router;
--- routes/deals.js
+++ routes/deals.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {isDealOwner} = require('../config/passport')
+const {isDealOwner,isAuth} = require('../config/passport')
 
 const {getAllDeals, createDeal, getDealByID, updateDealByID, deleteDealByID} = require('../controllers/deals');
 
-router.route('/api/deals').get(getAllDeals).post(createDeal);
-router.route('/api/deals/:id').get(getDealByID).patch(isDealOwner,updateDealByID).delete(isDealOwner,deleteDealByID);
+router.route('/api/deals').get(isAuth,getAllDeals).post(isAuth,createDeal);
+router.route('/api/deals/:id').get(isAuth,getDealByID).patch(isDealOwner,updateDealByID).delete(isDealOwner,deleteDealByID);
 
 module.exports = router;
