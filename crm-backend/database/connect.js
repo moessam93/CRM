737e79396be5025700fb7250ea63c7b7dfd8254d
Mohamed Essam <moessam@Mohameds-Air.home>
2022-03-09 06:05:42 +0200

    configure connection timezone

--- crm-backend/database/connect.js
+++ crm-backend/database/connect.js
@@ -6,7 +6,8 @@ const crmDB = mysql.createConnection({
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:'crm',
-    multipleStatements:'true'
+    multipleStatements:'true',
+    timezone : "+00:00"
 });
 
 module.exports = crmDB;
